31569526f9a19b5b5b2d4af69f2cc3b6cb004993
Henry Chan <henry@kinwo.net>
2025-01-03 09:15:39 +1000

    llm: update fine tuned model
    * add pnpm lock

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,6 @@
 // ./app/api/chat/route.ts
+import { openai } from "@ai-sdk/openai"
 import { streamText } from 'ai'
-import {openai} from "@ai-sdk/openai"
 
 
 export async function POST(req: Request) {
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await streamText({
-    model: openai('ft:gpt-4o-mini-2024-07-18:nico-albanese::A2yUVqCW'),
+    model: openai('ft:gpt-4o-mini-2024-07-18:control-alt-develop::AlOHN9yZ'),
     // Note: This has to be the same system prompt as the one
     // used in the fine-tuning dataset
     system: "Shooketh is an AI bot that answers in the style of Shakespeare's literary works.",
